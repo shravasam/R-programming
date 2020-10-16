2c2a03949087cb46edf87e0185414007f21f07ac
shravasam <sravan.vasam@yahoo.fr>
2020-10-16 17:04:23 +0200

    repeat-while-for

--- loops.r
+++ loops.r
@@ -1,3 +1,6 @@
+
+#repeat
+
 v <- c("Hello","loop")
 cnt <- 2
 repeat{
@@ -6,4 +9,20 @@ repeat{
   if(cnt > 5){
     break
   }
+}
+
+#while loop
+
+v <- c("Hello","while loop")
+cnt <- 2
+while (cnt < 7){
+  print(v)
+  cnt = cnt + 1
+}
+
+#for loop
+
+v <- LETTERS[1:26]
+for ( i in v) {
+  print(i)
 }
